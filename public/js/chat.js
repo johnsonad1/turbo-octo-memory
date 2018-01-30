acba1458b499c621c54cbede797b81c95025a6db
Andrew Johnson <andrew.johnson612@gmail.com>
2018-01-29 19:26:54 -0600

    Add join page that submits to chat.html

--- public/js/chat.js
+++ public/js/chat.js
@@ -13,7 +13,7 @@ function scrollToBottom () {
 	var newMessageHeight = newMessage.innerHeight();
 	var lastMessageHeight = newMessage.prev().innerHeight();
 
-	if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight>= scrollHeight) {
+	if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
 		messages.scrollTop(scrollHeight);
 	}
 }
